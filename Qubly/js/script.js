0e21090c1f7c5527cca62a76f4249da931ea1062
MarkReps <98027563+MarkReps@users.noreply.github.com>
2024-07-21 19:25:19 +0300

    finished main part of template (need fix swiper)

--- Qubly/js/script.js
+++ Qubly/js/script.js
@@ -22,3 +22,56 @@ menuBtns.forEach((btn)=>{
         }
     })
 })
+
+const acc = document.querySelectorAll(".column-top-footer__title");
+
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+const company = ['tonkean','bigSpring','clearbit','mine','shake'];
+new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    // loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.testimonial__pagination',
+      clickable:true,
+      bulletClass:".testimonial__item",
+      bulletActiveClass:".active",
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '"><svg><use xlink:href="img/sprite.svg#'+company[index]+'"></use></svg></span>';
+    },
+    },
+    
+})
+
+
+function onSuccess(googleUser) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+  }
+  function onFailure(error) {
+    console.log(error);
+  }
+  function renderButton() {
+    gapi.signin2.render('my-signin2', {
+      'scope': 'profile email',
+      'width': 240,
+      'height': 50,
+      'longtitle': true,
+      'theme': 'dark',
+      'onsuccess': onSuccess,
+      'onfailure': onFailure
+    });
+  }
